31247899b29a29a3ac1d87e382d2a1a04ed99580
Muhammad Uzair <muhammad.uzair@hayo.net>
2025-09-29 18:02:09 +0500

    reload the db

--- database.py
+++ database.py
@@ -525,4 +525,4 @@ def fetch_authorized_sender_emails(active_only: bool = True) -> List[str]:
 
     return emails
 
-insert_authorized_senders(VERIFIED_SENDERS)
+# insert_authorized_senders(VERIFIED_SENDERS)
